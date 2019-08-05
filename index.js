c6bdb57a20b2b8f773bc50538c6219df42dde090
Rob Cabacungan <robert.cabacungan@oath.com>
2019-08-05 11:52:51 -0400

    test(index.spec.js): define test globals to fix standardjs lint

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-function addTwo(a) { return a + 2; }
-module.exports = { addTwo };
+function addTwo (a) { return a + 2 }
+module.exports = { addTwo }
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,8 @@
-//import * as wdjtlb from './index'
-const wdjtlb = require('./index');
+/* global describe, test, expect */
+const wdjtlb = require('./index')
 
 describe('exported api of wdjtlb', () => {
   test('addTwo() adds 2 to argument', () => {
-    expect(wdjtlb.addTwo(3)).toBe(5);
-  });
-});
+    expect(wdjtlb.addTwo(3)).toBe(5)
+  })
+})
